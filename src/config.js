83f87412af514ed97e7e4becc736b8f1f83a087a
Amber Meritt <amber.merittcampbell@gmail.com>
2019-12-17 22:30:10 -0700

    updated config DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || `postgresql://postgres:G0d3sdeveloper@localhost/pawprint`, //add database
+  DATABASE_URL: process.env.DATABASE_URL || `postgresql://postgres:G0d3sdeveloper@localhost/pawprint`, //add database
   JWT_SECRET: process.env.JWT_SECRET || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
 }
