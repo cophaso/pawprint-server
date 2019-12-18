0533756183bf39eeddd615324a6f770ac4838906
cophaso <cophaso@gmail.com>
2019-12-17 19:51:47 -0800

    users api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const usersRouter = require('./users/users-router')
 
 const { NODE_ENV } = require('./config')
 
@@ -19,6 +20,7 @@ app.use(cors())
 app.get('/', (req,res) => {
   res.send('Hello, world!')
 })
+app.use('/api/users', usersRouter)
 
 app.use(function errorHandler(error, req, res, next) {
    let response
--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/pawprint',
+  JWT_SECRET: process.env.JWT_SECRET || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 }
