a7a98c11b40668d4491194470cfaa20f2aeb1db1
Amber Meritt <amber.merittcampbell@gmail.com>
2020-01-02 22:08:01 -0700

    updated syntax

--- test/pups-services-endpoints.spec.js
+++ test/pups-services-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe.only('Pups Services Endpoints', function() {
+describe('Pups Services Endpoints', function() {
     let db 
     
     let fillPupServices = [
